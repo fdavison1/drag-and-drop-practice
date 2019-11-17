598d7ae24c8e7bd59730ac741c64df1def24765d
fdavison1 <fred.noreply@gmail.com>
2019-11-17 10:36:58 -0700

    list order preserved

--- src/index.js
+++ src/index.js
@@ -13,6 +13,40 @@ class App extends React.Component {
 
     onDragEnd = result => {
         //TODO: reorder our column
+        const { destination, source, draggableId } = result
+
+        //if no destination, nothing we need to do
+        if(!destination){
+            return
+        }
+
+        //check to see if location has changed, if dropped in original location no action required
+        if (
+            destination.draggableId === source.droppableId &&
+            destination.index === source.index
+        ){
+            return
+        }
+
+        const column = this.state.columns[source.droppableId]
+        const newTasksIds = Array.from(column.taskIds)
+        newTasksIds.splice(source.index, 1)
+        newTasksIds.splice(destination.index, 0, draggableId)
+
+        const newColumn = {
+            ...column, 
+            taskIds: newTasksIds,
+        }
+
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns, 
+                [newColumn.id]: newColumn
+            }
+        }
+
+        this.setState(newState)
     }
 
 
